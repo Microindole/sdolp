dfcbf2a4aaae8d41c123c0b8a53b6bda9e618c51
程熙 张 <1563245867@qq.com>
2025-09-08 09:27:29 +0800

    init

---
+++ src/main/java/org/csu/sdolp/SdolpApplication.java
@@ -0,0 +1,13 @@
+package org.csu.sdolp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdolpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdolpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/sdolp/SdolpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.sdolp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdolpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
